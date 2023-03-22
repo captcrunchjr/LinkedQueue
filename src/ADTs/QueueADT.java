e47b92d0b3f93588b2ede942c4b2a40ae78d4409
Coding Rooms <git@codingrooms.io>
2023-03-22 17:24:50 +0000

    removed exception

--- src/ADTs/QueueADT.java
+++ src/ADTs/QueueADT.java
@@ -1,10 +1,9 @@
 package ADTs;
 
 import Exceptions.EmptyCollectionException;
-import Exceptions.QueueOverflowException;
 
 public interface QueueADT<T> extends CollectionADT<T> {
-    public void enqueue(T element) throws QueueOverflowException;
+    public void enqueue(T element);
 
     public T dequeue() throws EmptyCollectionException;
 
